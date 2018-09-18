a1b7d7e2778f6b348f102bf8cd5ca5e190a012dc
aromot <aromot@users.noreply.github.com>
2018-09-18 12:36:28 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+var cacheName = 'fiatlix-pwa-1-4';
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
